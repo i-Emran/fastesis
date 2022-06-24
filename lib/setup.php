0f810aedf01b80743781ebad05fc4f7b71a2c402
Imran Raza <44391976+iimranraza@users.noreply.github.com>
2022-06-25 03:02:32 +0530

    Update setup.php

--- lib/setup.php
+++ lib/setup.php
@@ -1,1 +1,26 @@
 <?php
+/*
+ * Fastesis
+ *
+ * @package     fastesis\lib\setup
+ *
+ * @link        https://hindistaan.com
+ * 
+ * @author      Imran Raza
+ * 
+ * @copyright   Copyright © 2022 Hindistaan
+ * 
+ * @license     GPL-3.0-or-later
+ * 
+ * @description Define & Setup theme before activation.
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+if ( ! defined( 'ABSPATH' ) ) {
+ exit;
+}
+define( 'CHILD_THEME_NAME', 'Fastesis' );
+define( 'CHILD_THEME_URL', 'https://hindistaan.com/' );
+define( 'CHILD_THEME_VERSION', '1.0' );
