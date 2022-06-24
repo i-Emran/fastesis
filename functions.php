444785c8c0c04328db43b25095ca667589db3f7b
Imran Raza <44391976+iimranraza@users.noreply.github.com>
2022-06-25 02:03:59 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,29 @@
 <?php
+/*
+ * Fastesis
+ *
+ * @package     fastesis\functions
+ *
+ * @link        https://hindistaan.com
+ * 
+ * @author      Imran Raza
+ * 
+ * @copyright   Copyright © 2022 Hindistaan
+ * 
+ * @license     GPL-3.0-or-later
+ * 
+ * @description The main functions of fastesis theme.
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+if ( ! defined( 'ABSPATH' ) ) {
+ exit;
+}
+
+
+// Start The Main Engine
+require_once(TEMPLATEPATH.'/lib/init.php');
+// Start The Child Engine
+require_once(STYLESHEETPATH.'/lib/init.php');
