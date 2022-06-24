76622dd23c2737b9c463acf430596b7266780631
Imran Raza <44391976+iimranraza@users.noreply.github.com>
2022-06-25 03:30:13 +0530

    Update optimize.php

--- lib/optimize.php
+++ lib/optimize.php
@@ -31,6 +31,19 @@ function _remove_script_version($src)
 }
 
 
+// Remove WP Version From Styles	
+add_filter( 'style_loader_src', 'sdt_remove_ver_css_js', 9999 );
+
+// Remove WP Version From Scripts
+add_filter( 'script_loader_src', 'sdt_remove_ver_css_js', 9999 );
+
+// Remove version numbers
+function sdt_remove_ver_css_js( $src ) {
+	if ( strpos( $src, 'ver=' ) )
+		$src = remove_query_arg( 'ver', $src );
+	return $src;
+}
+
 add_action('init', 'optimization');
 function optimization()
 {
