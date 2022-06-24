66218689b31bb050ba3b925e59b72cd1558a2e90
Imran Raza <44391976+iimranraza@users.noreply.github.com>
2022-06-25 02:40:14 +0530

    Update authorbox.php

--- lib/authorbox.php
+++ lib/authorbox.php
@@ -22,5 +22,22 @@ if ( ! defined( 'ABSPATH' ) ) {
  exit;
 }
 
+add_filter( 'genesis_author_box', 'custom_author_box', 10, 6 );
+function custom_author_box( $output, $context, $pattern, $gravatar, $title, $description ) {
+  
+  $name = get_the_author();
+  $title = get_the_author_meta( 'title' );
+	$output = '';
+	
+	$output .= '<div class="author-box">';
+	$output .= get_avatar( get_the_author_meta( 'email' ), 100 );
+		if( !empty( $title ) )
+		$name .= ', ' . $title;
+		$output .= '<h4 class="author-box-title"><a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '">' . $name . '</a></h4>';
+		$output .= '<p class="author-box-description">' . get_the_author_meta( 'description' ) . '</p>';
+		$output .= '</div>';
+	
+	return $output;
+}
 
 
