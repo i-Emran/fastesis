3fa1799ac96cba95e8c3d6fc3ba680635a9a0a94
Imran Raza <44391976+iimranraza@users.noreply.github.com>
2022-06-26 02:30:40 +0530

    Update functionality.php

--- lib/functionality.php
+++ lib/functionality.php
@@ -1,1 +1,22 @@
 <?php
+/*
+ * Fastesis
+ *
+ * @package     fastesis\lib\functionality
+ *
+ * @link        https://hindistaan.com
+ * 
+ * @author      Imran Raza
+ * 
+ * @copyright   Copyright © 2022 Hindistaan
+ * 
+ * @license     GPL-3.0-or-later
+ * 
+ * @description Custom Functionality.
+ */
+
+
+
+remove_action( 'genesis_entry_content', 'genesis_do_singular_image', 8 );
+add_action( 'genesis_[your_choice]', 'genesis_do_singular_image' );
+
